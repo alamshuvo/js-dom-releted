1a12ede2c160d675efdf938f3de21bb9d3890eed
iftakhar <iftakharalamshuvo11@gmail.com>
2024-02-10 23:22:50 +0600

    event

---
+++ module 2 even handaler/event.js
@@ -0,0 +1,15 @@
+function makeRed(){
+    document.body.style.backgroundColor='red';
+}
+// option 3
+const makeblue=document.getElementById('make-blue');
+makeblue.onclick=makeBlue;
+function makeBlue() {
+    document.body.style.backgroundColor='blue';
+}
+// option4
+const makePurple=document.getElementById('make-purple');
+makePurple.onclick=function makePurple() {
+    document.body.style.backgroundColor='purple'
+}
+
