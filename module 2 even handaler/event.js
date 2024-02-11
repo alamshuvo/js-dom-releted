b9e0c57c40a145c4f6de43fdb599372aaf7da8bd
iftakhar <iftakharalamshuvo11@gmail.com>
2024-02-11 12:37:00 +0600

    others

--- module 2 even handaler/event.js
+++ module 2 even handaler/event.js
@@ -12,4 +12,19 @@ const makePurple=document.getElementById('make-purple');
 makePurple.onclick=function makePurple() {
     document.body.style.backgroundColor='purple'
 }
+// option5
+const makePink=document.getElementById('make-pink');
+makePink.addEventListener('click', makePinka)
 
+function makePinka() {
+    document.body.style.backgroundColor='pink';
+}
+const makeGreen=document.getElementById('make-green');
+makeGreen.addEventListener('click',function makeGreena() {
+    document.body.style.backgroundColor='green';
+})
+
+// option 4 er final.
+document.getElementById("golden-rod").addEventListener('click',function () {
+ document.body.style.backgroundColor='goldenrod';   
+})
